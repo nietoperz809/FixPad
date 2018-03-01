b84f4b7b69478a14a44851d13af24935f009a564
Administrator <pbull809@gmail.com>
2018-03-01 09:16:23 +0100

    Initial commit

---
+++ src/FileManager.java
@@ -0,0 +1,100 @@
+import javax.swing.*;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.OpenOption;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class FileManager implements Runnable
+{
+    private final ArrayList<JEditorPane> list = new ArrayList<>();
+    private final String homePath = System.getProperty("user.home");
+    private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+
+    public void put(JEditorPane jp)
+    {
+        list.add(jp);
+    }
+
+    public void start()
+    {
+        loadEditors();
+        scheduler.scheduleAtFixedRate(this, 0, 10, TimeUnit.SECONDS);
+    }
+
+    public void stop()
+    {
+        scheduler.shutdown();
+        saveEditors();
+    }
+
+
+    private String load (String fname)
+    {
+        try
+        {
+            return new String(Files.readAllBytes(Paths.get(fname)));
+        }
+        catch (IOException e)
+        {
+            return null;
+        }
+    }
+
+    private boolean save (String txt, String fname)
+    {
+        try
+        {
+            byte[] bytes = txt.getBytes();
+            OpenOption[] op = {StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING};
+            Files.write (Paths.get(fname), bytes, op);
+            return true;
+        }
+        catch (IOException e)
+        {
+            System.out.println("save fail");
+            return false;
+        }
+    }
+
+    private void loadEditors()
+    {
+        for (int n=0; n<list.size(); n++)
+        {
+            JEditorPane jp = list.get(n);
+            String fname = createFname(n);
+            String txt = load (fname);
+            jp.setText(txt);
+        }
+    }
+
+    private void saveEditors()
+    {
+        for (int n=0; n<list.size(); n++)
+        {
+            JEditorPane jp = list.get(n);
+            String fname = createFname(n);
+            String txt = jp.getText();
+            String old = load (fname);
+            if (old == null || !txt.equals(old))
+            {
+                save (txt, fname);
+            }
+        }
+    }
+
+    private String createFname (int n)
+    {
+        return homePath + File.separator + "pane" + n;
+    }
+
+    public void run()
+    {
+        saveEditors();
+    }
+}
